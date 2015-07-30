d920e4f18f9a2419aee8de465dae457892790e1d
Ben Newsom <ben.newsom@gocanvas.com>
2015-07-30 00:28:59 +0000

    addedRails12FactorToGemfile

--- Gemfile
+++ Gemfile
@@ -35,5 +35,6 @@ end
 
 group :production do
   gem 'pg',           '~> 0.18'
+  gem 'rails_12factor'
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,15 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-Company.create(company_name: 'ACME CO.')
-Company.create(company_name: 'Wayne Industries')
+#Company.create(company_name: 'ACME CO.')
+#Company.create(company_name: 'Wayne Industries')
 
-Department.create(department_name: 'ACME Department', company: Company.first)
-Department.create(department_name: 'Wayne Department', company: Company.second)
+#Department.create(department_name: 'ACME Department', company: Company.first)
+#Department.create(department_name: 'Wayne Department', company: Company.second)
 
-# Employee.create
+#Employee.create(first_name: 'Road', last_name: 'Runner', position_title: 'Boss', gender: 'Female', 
+                #date_of_birth: '01/01/1950', employee_hire_date: '01/01/2014', department: Department.first)
+#Employee.create(first_name: 'Bruce', last_name: 'Wayne', position_title: 'Boss', gender: 'Male', 
+                #date_of_birth: '01/01/1970', employee_hire_date: '01/01/2015', department: Department.second)
 
 
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
